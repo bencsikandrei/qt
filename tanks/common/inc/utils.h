24ec888a9fe505b6f9a2cb2fbf851c8512c2652b
Andrei BENCSIK <andrei.bencsik@gmail.com>
2018-03-04 11:09:50 +0100

    [FT004] Refactored position to adapt to Vehicle needs (added tests)

--- tanks/common/inc/utils.h
+++ tanks/common/inc/utils.h
@@ -1,7 +1,21 @@
 #pragma once
 
+// standard libs
 #include <string>
 #include <memory>
 #include <cstdint>
 #include <iostream>
 #include <sstream>
+
+// project specific
+#include <position.h>
+
+namespace common {
+
+using Health = uint64_t;
+using Damage = Health; // damage is the opposite of health
+
+constexpr Health DEFAULT_VEHICLE_HEALTH = 100;
+
+
+} // common
